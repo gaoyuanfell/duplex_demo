e8cef2f29f1df747117fd7305e5aa1f9bbfe4998
moka <gaoyuanfell@sina.com>
2020-07-28 15:09:03 +0800

    first

---
+++ packages/ws-server/server.ts
@@ -0,0 +1,24 @@
+import ws, { Server as WsServer } from "ws";
+import express from "express";
+import https from "https";
+import fs from "fs";
+const app = express();
+
+const options = {
+  key: fs.readFileSync("./privatekey.pem"),
+  cert: fs.readFileSync("./certificate.pem"),
+};
+
+const server = https.createServer(options, app);
+
+const wss: WsServer = new ws.Server({ server });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.info(message);
+  });
+});
+
+server.listen(8081, () => {
+  console.info("WebSocket server start 8081 port");
+});
